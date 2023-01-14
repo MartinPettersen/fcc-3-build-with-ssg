480cde7c88ade875b06865a6ad9c3fb72f9c36bd
MartinPettersen <searchingtheskyfor@gmail.com>
2023-01-14 13:21:41 +0100

    adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+require('dotenv').config();
+
+//60d5798ebc72445abfd8f5b92bf134b0
+
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
